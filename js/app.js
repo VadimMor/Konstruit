944a5a85208898979c68822cf5974462b2dfa4d6
VadimMor <87434747+VadimMor@users.noreply.github.com>
2021-12-03 05:02:58 +0500

    animations

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ window.addEventListener('scroll', function() {
     let introHeight = document.getElementById('intro').clientHeight;
     let header = document.getElementById('header');
 
-    if (windowScroll >= introHeight+header.clientHeight) {
+    if (windowScroll >= introHeight) {
         header.classList.add('fixed');
         document.getElementById('about').style.marginTop = 124 + 'px';
     } else {
@@ -56,12 +56,47 @@ window.addEventListener('scroll', function() {
 // Меню нажатие 
 let navToggle = document.getElementById('nav_toggle');
 
-navToggle.addEventListener('click', function(element) {
-    let nav
+if (navToggle) {
+    navToggle.addEventListener('click', function(element) {
+        element.preventDefault();
 
-    element.preventDefault();
+        this.classList.add('fixed');
+        document.getElementById('nav').classList.add('fixed');
+    });
+}
 
-    this.classList.add('fixed');
-    document.getElementById('nav').classList.add('fixed');
-});
 
+// Анимация
+let animationItems = document.querySelectorAll('.anim-items');
+
+if (animationItems.length > 0) {
+    window.addEventListener('scroll', animationOnScroll);
+
+    function animationOnScroll() {
+        for (let i=0; i<animationItems.length; i++) {
+            const animationItem = animationItems[i];
+            const animationItemHeight = animationItem.offsetHeight;
+            const animationItemOffset = offset(animationItem).top
+            const animationStart = 4;
+
+            let animationPoint = window.innerHeight - animationItemHeight / animationStart;
+
+            if (animationItemHeight > window.innerHeight) {
+                animationPoint = window.innerHeight - window.innerHeight / animationStart;
+            }
+
+            if ((pageYOffset > animationItemOffset - animationPoint) && (pageYOffset < animationItemOffset - animationItemHeight)) {
+                animationItem.classList.add('_active');
+            }
+        }   
+    }
+
+    function offset(element) {
+        const rect = element.getBoundingClientRect(),
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        return {top: rect.top + scrollTop}
+    }
+
+    animationOnScroll();
+}
